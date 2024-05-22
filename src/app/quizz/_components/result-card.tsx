3fbcf91e55cc7c16fed573b108a9944b1c4b049f
Marius Chitaru <marius@chitaru.gmail.com>
2024-05-22 18:53:37 +0300

    neo buttons ui

---
+++ src/app/quizz/_components/result-card.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import clsx from "clsx";
+import React from "react";
+
+type Props = {
+  isCorrect: boolean | null;
+  correctAnswer: string | undefined;
+};
+
+const ResultCard = (props: Props) => {
+  const { isCorrect } = props;
+
+  if (isCorrect === null) {
+    return null;
+  }
+
+  const text = isCorrect
+    ? "Correct!"
+    : "Incorrect! The correct answer is: " + props.correctAnswer;
+
+  const borderClasses = clsx({
+    "border border-green-500": isCorrect,
+    "border border-red-500": !isCorrect,
+  });
+
+  return (
+    <div
+      className={cn(
+        borderClasses,
+        "border-2 rounded-lg p-4 text-center text-lg font-semibold m-4 bg-secondary"
+      )}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default ResultCard;
--- src/app/quizz/page.tsx
+++ src/app/quizz/page.tsx
@@ -4,32 +4,33 @@ import ProgressBar from "@/components/progress-bar";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, X } from "lucide-react";
 import { useState } from "react";
+import ResultCard from "./_components/result-card";
 
 const questions = [
   {
     text: "What is React?",
     answers: [
       {
-        text: "A library for building user interfaces", 
-        isCorrect: true, 
-        id: 1
+        text: "A library for building user interfaces",
+        isCorrect: true,
+        id: 1,
       },
       {
-        text: "A front-end framework", 
-        isCorrect: false, 
-        id: 2
+        text: "A front-end framework",
+        isCorrect: false,
+        id: 2,
       },
       {
-        text: "A back-end framework", 
-        isCorrect: false, 
-        id: 3
+        text: "A back-end framework",
+        isCorrect: false,
+        id: 3,
       },
       {
         text: "A database",
         isCorrect: false,
-        id: 4
-      }
-    ]
+        id: 4,
+      },
+    ],
   },
   {
     text: "What is JSX?",
@@ -37,24 +38,24 @@ const questions = [
       {
         text: "JavaScript XML",
         isCorrect: false,
-        id: 1
+        id: 1,
       },
       {
         text: "JavaScript",
         isCorrect: false,
-        id: 2
+        id: 2,
       },
       {
         text: "JavaScript and XML",
         isCorrect: false,
-        id: 3
+        id: 3,
       },
       {
         text: "JavaScript and HTML",
         isCorrect: false,
-        id: 4
-      }
-    ]
+        id: 4,
+      },
+    ],
   },
   {
     text: "What is the Virtual DOM?",
@@ -62,12 +63,12 @@ const questions = [
       {
         text: "A virtual representation of the DOM",
         isCorrect: true,
-        id: 1
+        id: 1,
       },
       {
         text: "A real DOM",
         isCorrect: false,
-        id: 2
+        id: 2,
       },
       {
         text: "A virtual representation of the browser",
@@ -77,11 +78,11 @@ const questions = [
       {
         text: "A virtual representation of the server",
         isCorrect: false,
-        id: 4
-      }
-    ]
-  }
-]
+        id: 4,
+      },
+    ],
+  },
+];
 
 export default function Home() {
   const [started, setStarted] = useState<boolean>(false);
@@ -91,69 +92,95 @@ export default function Home() {
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 
   const handleNext = () => {
-    if(!started){
+    if (!started) {
       setStarted(true);
       return;
     }
 
-    if(currentQuestion < questions.length - 1){
+    if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
     }
 
     setSelectedAnswer(null);
     setIsCorrect(null);
-  }
+  };
 
   const handleAnswer = (answer) => {
-
     setSelectedAnswer(answer.id);
     const isCurrentCorrect = answer.isCorrect;
-    if(isCurrentCorrect){
+    if (isCurrentCorrect) {
       setScore(score + 1);
     }
     setIsCorrect(isCurrentCorrect);
-
-  }
+  };
 
   return (
     <div className="flex flex-col flex-1">
       <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
         <header className="grid grid-cols-[auto,1fr,auto] grid-flow-col items-center justify-between py-2 gap-2">
-          <Button size={"icon"} variant={"outline"}><ChevronLeft /></Button>
-          <ProgressBar value={(currentQuestion/questions.length)*100}/>
-          <Button size={"icon"} variant={"outline"}><X /></Button>
+          <Button
+            size={"icon"}
+            variant={"outline"}
+          >
+            <ChevronLeft />
+          </Button>
+          <ProgressBar value={(currentQuestion / questions.length) * 100} />
+          <Button
+            size={"icon"}
+            variant={"outline"}
+          >
+            <X />
+          </Button>
         </header>
       </div>
       <main className="flex flex-1 justify-center">
-        {!started ? 
-        <h1 className="text-3xl font-bold">Hello World👋</h1> :
-        <div>
-          <h2 className="text-3xl font-bold">{questions[currentQuestion].text}</h2>
-          <div className="grid grid-cols-1 gap-6 mt-6">
-            {
-              questions[currentQuestion].answers.map((answer) => {
+        {!started ? (
+          <h1 className="text-3xl font-bold">Hello World👋</h1>
+        ) : (
+          <div>
+            <h2 className="text-3xl font-bold">
+              {questions[currentQuestion].text}
+            </h2>
+            <div className="grid grid-cols-1 gap-6 mt-6">
+              {questions[currentQuestion].answers.map((answer) => {
+                const variant =
+                  selectedAnswer === answer.id
+                    ? answer.isCorrect
+                      ? "neoSuccess"
+                      : "neoDanger"
+                    : "neoOutline";
                 return (
-                  <Button 
-                    key={answer.id} 
-                    variant={"secondary"}
+                  <Button
+                    key={answer.id}
+                    variant={variant}
+                    size={"xl"}
                     onClick={() => handleAnswer(answer)}
                   >
-                    {answer.text}
+                    <p className="whitespace-normal">{answer.text}</p>
                   </Button>
-                )
-              })
-            }
+                );
+              })}
+            </div>
           </div>
-        </div>}
+        )}
       </main>
       <footer className="footer pb-9 px-6 relative mb-0">
-        <p>{isCorrect ? "correct": "incorrect"}</p>
-        <Button 
+        <ResultCard
+          isCorrect={isCorrect}
+          correctAnswer={
+            questions[currentQuestion].answers.find(
+              (answer) => answer.isCorrect === true
+            )?.text
+          }
+        />
+        <Button
+          variant={"neo"}
+          size={"lg"}
           onClick={handleNext}
         >
           {!started ? "Start" : "Next"}
-        </Button>  
+        </Button>
       </footer>
     </div>
-  )
+  );
 }
