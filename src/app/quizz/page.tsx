0865aa8fcb3b9932d4cb0b67a711827c24645ee9
Marius Chitaru <marius@chitaru.gmail.com>
2024-05-22 17:43:00 +0300

    quizz

--- src/app/quizz/page.tsx
+++ src/app/quizz/page.tsx
@@ -2,6 +2,7 @@
 
 import ProgressBar from "@/components/progress-bar";
 import { Button } from "@/components/ui/button";
+import { ChevronLeft, X } from "lucide-react";
 import { useState } from "react";
 
 const questions = [
@@ -83,8 +84,11 @@ const questions = [
 ]
 
 export default function Home() {
-  const [started, setStarted] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [started, setStarted] = useState<boolean>(false);
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 
   const handleNext = () => {
     if(!started){
@@ -95,13 +99,29 @@ export default function Home() {
     if(currentQuestion < questions.length - 1){
       setCurrentQuestion(currentQuestion + 1);
     }
+
+    setSelectedAnswer(null);
+    setIsCorrect(null);
+  }
+
+  const handleAnswer = (answer) => {
+
+    setSelectedAnswer(answer.id);
+    const isCurrentCorrect = answer.isCorrect;
+    if(isCurrentCorrect){
+      setScore(score + 1);
+    }
+    setIsCorrect(isCurrentCorrect);
+
   }
 
   return (
     <div className="flex flex-col flex-1">
       <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
-        <header>
+        <header className="grid grid-cols-[auto,1fr,auto] grid-flow-col items-center justify-between py-2 gap-2">
+          <Button size={"icon"} variant={"outline"}><ChevronLeft /></Button>
           <ProgressBar value={(currentQuestion/questions.length)*100}/>
+          <Button size={"icon"} variant={"outline"}><X /></Button>
         </header>
       </div>
       <main className="flex flex-1 justify-center">
@@ -113,7 +133,13 @@ export default function Home() {
             {
               questions[currentQuestion].answers.map((answer) => {
                 return (
-                  <Button key={answer.id} variant={"secondary"}>{answer.text}</Button>
+                  <Button 
+                    key={answer.id} 
+                    variant={"secondary"}
+                    onClick={() => handleAnswer(answer)}
+                  >
+                    {answer.text}
+                  </Button>
                 )
               })
             }
@@ -121,7 +147,12 @@ export default function Home() {
         </div>}
       </main>
       <footer className="footer pb-9 px-6 relative mb-0">
-        <Button onClick={handleNext}>{!started ? "Start" : "Next"}</Button>  
+        <p>{isCorrect ? "correct": "incorrect"}</p>
+        <Button 
+          onClick={handleNext}
+        >
+          {!started ? "Start" : "Next"}
+        </Button>  
       </footer>
     </div>
   )
